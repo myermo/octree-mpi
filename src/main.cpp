a15122656fe802d38d37dcae8d4177df8673c05f
GarciaBarreiro <garcia.barreiro@proton.me>
2023-11-24 13:21:14 +0100

    fix: density

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,127 @@
 #include <iomanip>
 #include <iostream>
 #include "TimeWatcher.hpp"
+#include "NeighborKernels/KernelFactory.hpp"
 
 namespace fs = std::filesystem;
 
-double density(const Octree* gOctree, double radius, const std::vector<Lpoint>* points)
+double density_cube(const Octree& gOctree, double radius, const std::vector<Lpoint>& points)
 {
-    unsigned long tot_neigh = 0;
+    double tot_dens = 0;
 
-    for (Lpoint point : *points) {
-        tot_neigh += gOctree->searchNeighbors3D(point, radius).size();
+    for (const Lpoint& point : points) {
+        tot_dens += gOctree.searchNeighbors3D(point, radius).size()/std::pow(radius*2,3);
     }
 
-    return tot_neigh/(double)points->size();
+    return tot_dens/points.size();
 }
 
-double density_2(const Octree* gOctree, double radius, const std::vector<Lpoint>* points)
+// con numNeighbors o tempo de cálculo é aprox. 80% que con searchNeighbors().size()
+double density_cube_2(const Octree& gOctree, double radius, const std::vector<Lpoint>& points)
 {
-    unsigned long tot_neigh = 0;
+    double tot_dens = 0;
 
-    for (Lpoint point : *points) {
-        tot_neigh += gOctree->numNeighbors(&point, radius);  // nesta funcion da erro
+    for (const Lpoint& point : points) {
+        tot_dens += gOctree.numNeighbors<Kernel_t::cube>(point, radius)/std::pow(radius*2,3);
     }
 
-    return tot_neigh/(double)points->size();
+    return tot_dens/points.size();
+}
+
+double density_sphere(const Octree& gOctree, double radius, const std::vector<Lpoint>& points)
+{
+    double tot_dens = 0;
+
+    for (const Lpoint& point : points) {
+        tot_dens += gOctree.searchSphereNeighbors(point, radius).size()/(M_PI*std::pow(radius,2));
+    }
+
+    return tot_dens/points.size();
+}
+
+double density_square(const Octree& gOctree, double radius, const std::vector<Lpoint>& points)
+{
+    double tot_dens = 0;
+
+    for (const Lpoint& point : points) {
+        tot_dens += gOctree.searchNeighbors2D(point, radius).size()/std::pow(radius*2,3);
+    }
+
+    return tot_dens/points.size();
+}
+
+double density_square_2(const Octree& gOctree, double radius, const std::vector<Lpoint>& points)
+{
+    double tot_dens = 0;
+
+    for (const Lpoint& point : points) {
+        tot_dens += gOctree.numNeighbors<Kernel_t::square>(point, radius)/std::pow(radius*2,3);
+    }
+
+    return tot_dens/points.size();
+}
+
+void time_diff(const Octree& gOctree, double radius, const std::vector<Lpoint>& points)
+{
+    TimeWatcher tw;
+
+    tw.start();
+    std::vector<Lpoint *> search_result;
+    for (const Lpoint& point : points) {
+        search_result = gOctree.searchNeighbors3D(point, radius);
+    }
+    tw.stop();
+    double time_search = tw.getElapsedDecimalSeconds();
+
+    tw.start();
+    unsigned long num_result;
+    for (const Lpoint& point : points) {
+        num_result = gOctree.numNeighbors<Kernel_t::cube>(point, radius);
+    }
+    tw.stop();
+    double time_num = tw.getElapsedDecimalSeconds();
+
+    std::cout << "Time to search neighbors: " << time_search << std::endl;
+    std::cout << "Time to get num neighbors: " << time_num << std::endl;
+
+    std::cout << search_result.size() - num_result << std::endl;
+}
+
+void tests(const Octree& gOctree, double radius, const std::vector<Lpoint>& points)
+{
+	TimeWatcher tw;
+    double dense;
+    std::cout << std::endl;
+
+    tw.start();
+    dense = density_cube(gOctree, radius, points);
+    std::cout << "Cube density with a " << radius << "m radius: " << dense << std::endl;
+    tw.stop();
+	std::cout << "Time to get density: " << tw.getElapsedDecimalSeconds() << " seconds\n";
+
+    tw.start();
+    dense = density_cube_2(gOctree, radius, points);
+    std::cout << "Cube density 2 with a " << radius << "m radius: " << dense << std::endl;
+    tw.stop();
+	std::cout << "Time to get density: " << tw.getElapsedDecimalSeconds() << " seconds\n";
+
+    tw.start();
+    dense = density_sphere(gOctree, radius, points);
+    std::cout << "Sphere density with a "<< radius <<"m radius: " << dense << std::endl;
+    tw.stop();
+	std::cout << "Time to get density: " << tw.getElapsedDecimalSeconds() << " seconds\n";
+
+    tw.start();
+    dense = density_square(gOctree, radius, points);
+    std::cout << "Square density with a "<< radius <<"m radius: " << dense << std::endl;
+    tw.stop();
+	std::cout << "Time to get density: " << tw.getElapsedDecimalSeconds() << " seconds\n";
+
+    tw.start();
+    dense = density_square_2(gOctree, radius, points);
+    std::cout << "Square density 2 with a "<< radius <<"m radius: " << dense << std::endl;
+    tw.stop();
+	std::cout << "Time to get density: " << tw.getElapsedDecimalSeconds() << " seconds\n";
 }
 
 int main(int argc, char* argv[])
@@ -62,17 +160,11 @@ int main(int argc, char* argv[])
 	tw.stop();
 	std::cout << "Time to build global octree: " << tw.getElapsedDecimalSeconds() << " seconds\n";
 
-    tw.start();
-    double dense = density(&gOctree, 1, &points);
-    tw.stop();
-    std::cout << "density with a 1m radius: " << dense << std::endl;
-	std::cout << "Time to get density: " << tw.getElapsedDecimalSeconds() << " seconds\n";
+    tests(gOctree, .5, points);
+    tests(gOctree, 1, points);
+    tests(gOctree, 2, points);
 
-    tw.start();
-    dense = density_2(&gOctree, 1, &points);
-    tw.stop();
-    std::cout << "density with a 1m radius: " << dense << std::endl;
-	std::cout << "Time to get density: " << tw.getElapsedDecimalSeconds() << " seconds\n";
+    for (int i = 0; i < 5; i++) { time_diff(gOctree, i + 1, points); }
 
 	return EXIT_SUCCESS;
 }
