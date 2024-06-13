ce629f9bf18ac638859e8a48c059d4a955df78f9
GarciaBarreiro <garcia.barreiro@proton.me>
2024-06-14 00:06:13 +0200

    feat: all geometric features, txt output

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <cmath>
 #include <mpi.h>
 #include <armadillo>
+#include <omp.h>
 
 #include "Box.hpp"  // temp
 
@@ -24,7 +25,7 @@ Lpoint centroid(const std::vector<Lpoint*>& points)
         y += p->getY();
         z += p->getZ();
     }
-    return Lpoint(x / points.size(), y / points.size(), z / points.size());
+    return {x / points.size(), y / points.size(), z / points.size()};
 }
 
 arma::mat cov(const std::vector<Lpoint*>& points)
@@ -39,6 +40,58 @@ arma::mat cov(const std::vector<Lpoint*>& points)
     return cov / points.size();
 }
 
+std::vector<double> features(const std::vector<Lpoint*>& neigh, const Lpoint& p) {
+        std::vector<double> feats(18);
+
+        // get eigenvalues
+        auto A = cov(neigh);
+        arma::vec eigval;
+        arma::mat eigvec;
+        arma::eig_sym(eigval, eigvec, A);
+        arma::vec z = { 0, 0, 1 };
+
+        double neighInv = 1.0 / neigh.size();
+
+        feats[0] = neigh.size();                                                 // number of neighbors
+        feats[1] = accu(eigval);                                                 // sum eigenvalues
+        feats[2] = pow(fabs((eigval[0] * eigval[1] * eigval[2])), 1.0 / 3.0);    // omnivariance
+        feats[3] = -accu(eigval % arma::log(eigval));                            // eigenentropy
+        feats[4] = (eigval[2] - eigval[1]) / eigval[2];                          // linearity
+        feats[5] = (eigval[1] - eigval[0]) / eigval[2];                          // planarity
+        feats[6] = eigval[0] / eigval[2];                                        // sphericity
+        feats[7] = eigval[0] / (eigval[0] + eigval[1] + eigval[2]);              // change of curvature
+        // verticality
+        feats[8] = fabs(M_PI / 2 - acos(arma::dot(eigvec.col(0), z) / sqrt(arma::dot(eigvec.col(0), eigvec.col(0)) * arma::dot(z, z))));
+        feats[9] = fabs(M_PI / 2 - acos(arma::dot(eigvec.col(2), z) / sqrt(arma::dot(eigvec.col(2), eigvec.col(2)) * arma::dot(z, z))));
+        // TODO:
+        // absolute moment 1 - 6, vertical moment 1 - 2
+        // https://isprs-annals.copernicus.org/articles/III-3/177/2016/isprs-annals-III-3-177-2016.pdf
+        std::vector<double> abs(6);
+        std::vector<double> vert(2);
+        for (const Lpoint* n : neigh)
+        {
+            arma::vec v = { n->getX() - p.getX(), n->getY() - p.getY(), n->getZ() - p.getZ() };
+            abs[0] += arma::dot(v, eigvec.col(0));
+            abs[1] += pow(arma::dot(v, eigvec.col(0)), 2);
+            abs[2] += arma::dot(v, eigvec.col(1));
+            abs[3] += pow(arma::dot(v, eigvec.col(1)), 2);
+            abs[4] += arma::dot(v, eigvec.col(2));
+            abs[5] += pow(arma::dot(v, eigvec.col(2)), 2);
+            vert[0] += arma::dot(v, z);
+            vert[1] += pow(arma::dot(v, z), 2);
+        }
+        feats[10] = neighInv * fabs(abs[0]);
+        feats[11] = neighInv * fabs(abs[1]);
+        feats[12] = neighInv * fabs(abs[2]);
+        feats[13] = neighInv * fabs(abs[3]);
+        feats[14] = neighInv * fabs(abs[4]);
+        feats[15] = neighInv * fabs(abs[5]);
+        feats[16] = neighInv * vert[0];
+        feats[17] = neighInv * vert[1];
+
+        return feats;
+}
+
 int main(int argc, char* argv[])
 {
 	setDefaults();
@@ -51,6 +104,7 @@ int main(int argc, char* argv[])
 
 	// Handling Directories
 	createDirectory(mainOptions.outputDirName);
+    mainOptions.outputDirName = mainOptions.outputDirName / (fileName + ".txt");
 
 	// Print three decimals
 	std::cout << std::fixed;
@@ -107,53 +161,53 @@ int main(int argc, char* argv[])
     MPI_Reduce(&localPoints, &totalPoints, 1, MPI_UNSIGNED, MPI_SUM, 0, MPI_COMM_WORLD);
     MPI_Reduce(&numOvl, &totalOvl, 1, MPI_UNSIGNED, MPI_SUM, 0, MPI_COMM_WORLD);
 
+    MPI_File file;
+    MPI_File_open(MPI_COMM_WORLD, mainOptions.outputDirName.c_str(), MPI_MODE_CREATE | MPI_MODE_WRONLY, MPI_INFO_NULL, &file);
+    // header for txt file (helps in CloudCompare)
+    if (node == 0) {
+        std::string str = "x y z node numNeigh sumEig omnivariance eigenentropy linearity planarity sphericity changeCurvature "
+            "verticality0 verticality2 absMom0 absMom0^2 absMom1 absMom1^2 absMom2 absMom2^2 vertMom vertMom^2\n";
+        MPI_File_write_shared(file, str.c_str(), str.size(), MPI_CHAR, MPI_STATUS_IGNORE);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+
     tw.start();
     double localNeigh = 0;
-    double sumEigVal = 0;
-    double omnivariance = 0;
+    #pragma omp parallel for reduction(+:localNeigh)
     for (const Lpoint& p : lPoints)
     {
         if (p.isOverlap()) { continue; }
         auto neigh = lOctree.searchNeighbors3D(p, mainOptions.radius);
+        if (neigh.size() == 0) { continue; }
 
-        // get eigenvalues
-        auto A = cov(neigh);
-        arma::cx_vec c_eigval;
-        arma::eig_gen(c_eigval, A);
+        auto feats = features(neigh, p);
 
-        // sort eigval and discard complex part
-        // doing it as arma::real(arma::sort(c_eigval)) raises an error
-        c_eigval = arma::sort(c_eigval);
-        arma::vec eigval = arma::real(c_eigval);
-
-        // some points had eigenvalues as [matrix size: 0x1]
-        if (eigval.n_rows != 3) { continue; }
-
-        sumEigVal += accu(eigval);
-        double lOmni = pow((eigval[0] * eigval[1] * eigval[2]), 1.0 / 3.0);
-        // else, result is -nan always
-        if (std::isnan(lOmni)) { continue; }
-        omnivariance += lOmni;
         localNeigh += neigh.size();
+
+        // print coordinates and other info to file "out.txt"
+        std::string str = std::to_string(p.getX()) + " " + std::to_string(p.getY()) + " " + std::to_string(p.getZ()) + " " + std::to_string(node);
+        for (double f : feats) { str += " " + std::to_string(f); }
+        str += "\n";
+
+        #pragma omp critical
+        MPI_File_write_shared(file, str.c_str(), str.size(), MPI_CHAR, MPI_STATUS_IGNORE);
     }
     tw.stop();
 
+    MPI_File_close(&file);
+
+    double localTime = tw.getElapsedDecimalSeconds();
     std::cout << "Node " << node << " Density for radius " << mainOptions.radius << ": " << localNeigh / (lPoints.size() - numOvl) << "\n"
-        << "    Mean sum eigenvalues: " << sumEigVal / (lPoints.size() - numOvl) << "\n"
-        << "    Mean omnivariance: " << omnivariance / (lPoints.size() - numOvl) << "\n"
-        << "    Time to calculate density: " << tw.getElapsedDecimalSeconds() << " seconds\n\n";
+        << "    Time to calculate density: " << localTime << " seconds\n\n";
 
     double totNeigh = 0;
     MPI_Reduce(&localNeigh, &totNeigh, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    double totSumEigVal = 0;
-    MPI_Reduce(&sumEigVal, &totSumEigVal, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-    double totOmni = 0;
-    MPI_Reduce(&omnivariance, &totOmni, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    double totTime = 0;
+    MPI_Reduce(&localTime, &totTime, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
     if (node == 0)
     {
         std::cout << "Density for radius " << mainOptions.radius << ": " << totNeigh / (totalPoints - totalOvl) << "\n"
-            << "Mean sum eigenvalues: " << totSumEigVal / (totalPoints - totalOvl) << "\n"
-            << "Mean omnivariance: " << totOmni / (totalPoints - totalOvl) << "\n";
+            << "    Mean time to calculate density: " << totTime / npes << " seconds\n";
     }
 
     // Sequential
@@ -181,9 +235,16 @@ int main(int argc, char* argv[])
         // Density
         tw.start();
         unsigned long totNeigh = 0;
+
+        #pragma omp parallel for reduction(+:totNeigh)
         for (const Lpoint& p : points)
         {
-            totNeigh += gOctree.searchNeighbors3D(p, mainOptions.radius).size();
+            auto neigh = gOctree.searchNeighbors3D(p, mainOptions.radius);
+            if (neigh.size() == 0) { continue; }
+
+            auto feats = features(neigh, p);
+
+            totNeigh += neigh.size();
         }
         tw.stop();
         std::cout << "Density for radius " << mainOptions.radius << ": " << static_cast<double>(totNeigh) / points.size() << "\n"
